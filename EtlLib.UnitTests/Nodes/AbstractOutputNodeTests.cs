dd604cdfd9266f3008afcc5442511ad565d0a4c9
cyle.witruk <cyle.witruk@ccwtechnologies.com>
2018-01-29 21:51:04 +0100

    Updated package metadata for project files and little bit of tests

---
+++ EtlLib.UnitTests/Nodes/AbstractOutputNodeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using EtlLib.Data;
+using EtlLib.Nodes;
+using EtlLib.Pipeline;
+using EtlLib.Support;
+using FluentAssertions;
+
+namespace EtlLib.UnitTests.Nodes
+{
+    public class AbstractOutputNodeTests
+    {
+        public void AbstractOutputNode_calls_SignalEnd_when_general_error()
+        {
+            var context = new EtlPipelineContext();
+            var errorHandler = new TestErrorHandler();
+            var nodeStatistics = new NodeStatistics();
+            var emitter = new TestEmitter<Row>();
+
+            var node = new TestAbstractOutputNode<Row>((ctx, e) =>
+            {
+                e.Emit(new Row());
+                throw new Exception("This is a test exception.");
+            });
+            node.SetErrorHandler(errorHandler);
+            node.SetEmitter(emitter);
+            nodeStatistics.RegisterNode(node);
+
+            node.Execute(context);
+
+            nodeStatistics.TotalWrites.Should().Be(1);
+            nodeStatistics.TotalErrors.Should().Be(1);
+            errorHandler.Errors.Count.Should().Be(1);
+            emitter.HasSignalledEnd.Should().BeTrue();
+        }
+
+        
+    }
+}
--- EtlLib.UnitTests/Nodes/GeneralNodeTests.cs
+++ EtlLib.UnitTests/Nodes/GeneralNodeTests.cs
@@ -4,9 +4,6 @@ namespace EtlLib.UnitTests.Nodes
 {
     public class GeneralNodeTests
     {
-        public void AbstractOutputNode_calls_SignalEnd_when_general_error()
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
