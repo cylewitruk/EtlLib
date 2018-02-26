1f110451eb7a45bec29603660791d05afa1634ff
cyle.witruk <cyle.witruk@paynova.com>
2018-02-26 20:52:46 +0100

    Added samples project (empty)

---
+++ samples/EtlLib.Samples.Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EtlLib.Logging.NLog;
+using EtlLib.Pipeline;
+
+namespace EtlLib.Samples.Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EtlLibConfig.LoggingAdapter = new NLogLoggingAdapter();
+
+            var context = new EtlPipelineContext();
+        }
+    }
+}
