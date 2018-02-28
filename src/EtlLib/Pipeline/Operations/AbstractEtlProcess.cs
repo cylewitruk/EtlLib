5ad13795cc8e467cb555008108df6255f94b7856
cyle.witruk <cyle.witruk@paynova.com>
2018-02-28 16:10:26 +0100

    Fixed issue with AbstractEtlProcess and context not following

--- src/EtlLib/Pipeline/Operations/AbstractEtlProcess.cs
+++ src/EtlLib/Pipeline/Operations/AbstractEtlProcess.cs
@@ -7,18 +7,22 @@ namespace EtlLib.Pipeline.Operations
     public abstract class AbstractEtlProcess : AbstractEtlOperationWithNoResult
     {
         private IEtlOperationWithNoResult _etlProcess;
+        private IEtlProcessBuilder _builder;
+        private Action<IEtlProcessBuilder> _bootstrapBuilder;
 
         protected void Build(Action<IEtlProcessBuilder> builder)
         {
-            var b = EtlProcessBuilder.Create();
-            builder(b);
-
-            _etlProcess = ((EtlProcessBuilder)b).Build();
-            Named(_etlProcess.Name);
+            _bootstrapBuilder = builder;
         }
 
         public override IEtlOperationResult Execute(EtlPipelineContext context)
         {
+            _builder = EtlProcessBuilder.Create(context);
+            _bootstrapBuilder(_builder);
+
+            _etlProcess = ((EtlProcessBuilder)_builder).Build();
+            Named(_etlProcess.Name);
+
             return _etlProcess.Execute(context);
         }
     }
