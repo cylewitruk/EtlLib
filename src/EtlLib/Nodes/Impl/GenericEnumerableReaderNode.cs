089c12bab023e69236e1b8f9fa1322dbc5bc6934
cyle.witruk <cyle.witruk@paynova.com>
2018-03-03 14:17:31 +0100

    Added GenericEnumerableReaderNode and other minor improvements

---
+++ src/EtlLib/Nodes/Impl/GenericEnumerableReaderNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using EtlLib.Data;
+using EtlLib.Pipeline;
+
+namespace EtlLib.Nodes.Impl
+{
+    public class GenericEnumerableReaderNode<T> : AbstractOutputNode<T> where T : class, INodeOutput<T>, new()
+    {
+        private readonly IEnumerable<T> _enumerable;
+
+        public GenericEnumerableReaderNode(IEnumerable<T> enumerable)
+        {
+            _enumerable = enumerable;
+        }
+
+        public override void OnExecute(EtlPipelineContext context)
+        {
+            foreach(var item in _enumerable)
+                Emit(item);
+
+            SignalEnd();
+        }
+    }
+}
